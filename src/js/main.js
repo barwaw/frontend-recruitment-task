948d6fb11a70bce893b7a28f92795fd6c39e622e
barwaw <you@example.com>
2022-06-14 14:57:11 +0200

    added modal click counting functionality

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,18 @@ const modal = document.querySelector('.modal')
 const modalExitButton = document.querySelector('.modal__exit')
 const modalCounter = document.querySelector('.modal__counter')
 const modalResetButton = document.querySelector('.modal__button')
-let clickCounter = 0
+
+let clickCounter = JSON.parse(localStorage.getItem('counter'))
+if(!clickCounter) localStorage.setItem('counter', 0)
 
 heroButton.addEventListener('click', e => {
   modal.classList.remove('disabled')
+  clickCounter += 1
+  localStorage.setItem('counter', clickCounter)
+  updateCounter()
+  if(clickCounter > 5) {
+    modalResetButton.classList.remove('disabled')
+  }
 })
 
 modalExitButton.addEventListener('click', e => {
@@ -16,3 +24,20 @@ modalExitButton.addEventListener('click', e => {
 modal.addEventListener('click', e => {
   if(e.target === modal) modal.classList.add('disabled')
 })
+
+modalResetButton.addEventListener('click', e => {
+  clickCounter = 0
+  localStorage.setItem('counter', clickCounter)
+  modalResetButton.classList.add('disabled')
+  updateCounter()
+})
+
+
+
+const updateCounter = () => {
+  if(clickCounter === 1) {
+    modalCounter.textContent = `${clickCounter} time`
+  } else {
+    modalCounter.textContent = `${clickCounter} times`
+  }
+}
